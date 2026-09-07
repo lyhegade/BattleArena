0e2b23ca37b860e970f52978c53ee436f6282ce7
lyhegade <lyhegade17@gmail.com>
2026-09-07 14:47:27 +0800

    Add Battle Loop And Special Attacks to Warrior Class

--- BattleArena/BattleArena/Program.cs
+++ BattleArena/BattleArena/Program.cs
@@ -1,33 +1,37 @@
-﻿using BattleArena.Warriors;
+﻿using BattleArena.Warrior;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BattleArena
+namespace BattleArena.Warrior
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Warrior Raymond = new Warrior("Raymond", 100, 30);
-            Warrior Kirk = new Warrior("Kirk", 150, 50);
-            Warrior Eloi = new Warrior("Eloi", 200, 40);
+            Warrior Raymond = new Warrior("Raymond", 100, 30, "tumakbo");
+            Warrior Kirk = new Warrior("Kirk", 150, 50, "hinabol");
+            Warrior Eloi = new Warrior("Eloi", 200, 40, "taguro");
 
+            Raymond.DisplayStatus();
+            Kirk.DisplayStatus();
+            Eloi.DisplayStatus();
 
-            Console.WriteLine($"Raymond: {Raymond.Name} has " +
-                $"{Raymond.Health} health and {Raymond.AttackPower}" + $" attack power.");
-            Console.WriteLine("----------------------------");
-
-            Console.WriteLine($"Kirk: {Kirk.Name} has " +
-                $"{Kirk.Health} health and {Kirk.AttackPower}" + $" attack power.");
-            Console.WriteLine("----------------------------");
-
-            Console.WriteLine($"Eloi: {Eloi.Name} has " +
-                $"{Eloi.Health} health and {Eloi.AttackPower}" + $" attack power.");
-            Console.WriteLine("----------------------------");
+            int round = 1;
+            while (Raymond.IsAlive && Kirk.IsAlive && Eloi.IsAlive)
+            {
+                Console.WriteLine("-------- Round {round}");
+                Raymond.Attack(Kirk);
+                Kirk.Attack(Eloi);
+                Eloi.Attack(Raymond);
+                Console.WriteLine("-----------------------");
+                round++;
+            }
             Console.ReadKey();
         }
     }
 }
+
+
